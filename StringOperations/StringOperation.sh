b7b88aa090bbd3144e6d1e55ae1d880be8518acf
puneetchugh <puneet.chugh@spectralink.com>
2018-07-27 16:35:27 -0600

    Removing duplicate file

--- StringOperations/StringOperation.sh
+++ StringOperations/StringOperation.sh
@@ -6,9 +6,14 @@ string_2="pankaj"
 echo "$string_1 length is : "${#string_1}
 echo "$string_2 length is : ${#string_2}"
 
+echo $string_1" length is : "${#string_1}
+
+#if [[ string_1 > string_2 ]] this doesnt compare the data in variables. Just compares string_1 with string_2
 if [[ $string_1 > $string_2 ]]
 then
 	echo "$string_1 comes after $string_2"
 else
 	echo "$string_1 comes before $string_2"
 fi
+
+
